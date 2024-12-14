ffad565ce3331eea7a6ce27b4b61e9b4e9b6c486
Yahani Nethmini <tynperera@gmail.com>
2024-12-14 15:34:47 +0530

    implementing auth logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,29 +15,37 @@ import ShoppingCheckout from "./pages/shopping-view/checkout"
 import ShoppingAccount from "./pages/shopping-view/account"
 
 function App() {
+
   return (
     <div className="flex flex-col overflow-hidden bg-white">
-      
-      {/* <h1>Header Components</h1> */}
-
       <Routes>
         <Route path="/auth" element={<AuthLayout />}>
+        {/* <CheckAuth>
+              <AuthLayout />
+            </CheckAuth> */}
           <Route path='login' element={<AuthLogin />} />
           <Route path='register' element={<AuthRegister />} />
         </Route> 
         <Route path="/admin" element={<AdminLayout/>}>
+        {/* <CheckAuth>
+            <AdminLayout/>
+          </CheckAuth> */}
           <Route path='dashboard' element={<AdminDashboard/>} />
           <Route path='products' element={<AdminProducts/>} />
           <Route path='orders' element={<AdminOrders/>} />
           <Route path='features' element={<AdminFeatures/>} />
         </Route>
         <Route path='/shop' element={<ShoppingLayout/>}>
+        {/* <CheckAuth>
+            <ShoppingLayout/>
+         </CheckAuth> */}
           <Route path='home' element={<ShoppingHome/>} /> 
           <Route path='listing' element={<ShoppingListing/>} />
           <Route path='checkout' element={<ShoppingCheckout/>} /> 
           <Route path='account' element={<ShoppingAccount/>} />
         </Route>
         <Route path='*' element={<NotFound/>} />
+        <Route path='/unauth-page' element={<NotFound/>} />
       </Routes>
     </div>
   )
